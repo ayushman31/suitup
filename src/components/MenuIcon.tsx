9a0393020f15a9148411ba2e5773221a1e3b5ff7
Ayushman Singh <ayushmans012@gmail.com>
2025-07-19 04:13:06 +0530

    menu icon updated

--- src/components/MenuIcon.tsx
+++ src/components/MenuIcon.tsx
@@ -1,9 +1,13 @@
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
-export default function MenuIcon() {
+interface MenuIconProps {
+  className?: string;
+}
+
+export default function MenuIcon({ className }: MenuIconProps) {
   return (
     <div className="fixed top-0 left-0 z-50 flex items-center justify-between p-10">
-        <HiOutlineMenuAlt4 className="text-white text-2xl" />
+        <HiOutlineMenuAlt4 className={` text-2xl ${className}`} />
       </div>
   );
 }
