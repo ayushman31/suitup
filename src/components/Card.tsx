777caf32a3134ce3ba6f3211e569c9e62c1b4859
Ayushman Singh <ayushmans012@gmail.com>
2025-07-20 00:41:41 +0530

    shadow added in card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -137,6 +137,8 @@ const Card: React.FC<CardProps> = ({
         overflow-hidden
         flex
         flex-col
+        shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]
+        cursor-pointer
       `}
       style={style}
       onMouseEnter={!isTouchDevice ? handleMouseEnter : undefined}
